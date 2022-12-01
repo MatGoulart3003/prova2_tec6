0649b3a4d98dd0fd2f2b1e152f7d46304c6b80d6
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2022-12-01 20:05:21 -0300

    sexto commit

--- prova2/src/router/router.js
+++ prova2/src/router/router.js
@@ -2,10 +2,14 @@ const express = require('express')
 const router = express.Router()
 const customerController = require('../controller/customer.controller')
 const productController = require('../controller/product.controller')
+const orderedController = require('../controller/ordered.controller')
 
 router.post('/customers', customerController.create)
 router.post('/products', productController.create)
+router.post('/ordereds', orderedController.create)
 router.delete('/customers/:id', customerController.remove)
 router.delete('/products/:id', productController.remove)
+router.delete('/ordereds/:id', orderedController.remove)
+
 
 module.exports = router
